cb9298ea2d95196825031a7c2d3cb1668b134eb7
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2024-10-13 14:04:05 -0500

    Add playground page to test components

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,9 +1,16 @@
+'use client';
+
+import { Button, Input } from '@/components/ui';
 import './page.css';
 
 export default function Playground() {
   return (
     <div className='container'>
       <h1>Playground</h1>
+      <Button onClick={() => alert('Hello!')}>Click me</Button>
+      <div className='input-wrapper'>
+        <Input placeholder='Enter your name...' />
+      </div>
     </div>
   );
 }
