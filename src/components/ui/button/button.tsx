855b1a2f1bd4944b38a7847e9c462f4b894db55e
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2024-10-13 15:09:57 -0500

    Add icon prop to Button component

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import type { LucideIcon } from 'lucide-react';
 import { PropsWithChildren } from 'react';
 import './button.css';
 
 type ButtonProps = PropsWithChildren & {
+  icon?: LucideIcon;
   onClick?: () => void;
 };
 
-export default function Button({ children, onClick }: ButtonProps) {
+export default function Button({ icon: Icon, children, onClick }: ButtonProps) {
   return (
     <button className='button' onClick={onClick}>
       {children}
+      {Icon && <Icon />}
     </button>
   );
 }
