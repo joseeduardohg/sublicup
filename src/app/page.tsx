2861250dfd09a7cc0bc4f7aff013ac79e0368a48
Jusit-Toledo <149429020+Jusit-Toledo@users.noreply.github.com>
2024-10-15 11:50:35 -0500

    Mover html home a su componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,79 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import './page.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+      <section className='seccion-principal'>
+        <section className='SP-textos'>
+          <h2>Si puedes imaginarlo podemos sublimarlo</h2>
+          <br />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab aliquam
+            vitae voluptate explicabo dolores quasi officia sequi asperiores,
+            illum libero.
+          </p>
+        </section>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <section>
+          <Image height={400} src='/src/images/taza-logo.jpeg' alt='' />
+        </section>
+      </section>
+
+      <section className='galeria'></section>
+
+      <section className='testimonios'>
+        <h2>Testimonios</h2>
+        <section className='testimonios-seccion'>
+          <section className='testimonios-perfiles'>
+            <Image height={400} src='/src/images/Testimonio2.jpg' alt='' />
+            <p>NOMBRE</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              aut!
+            </p>
+          </section>
+
+          <section className='testimonios-perfiles'>
+            <Image height={400} src='/src/images/Testimonio2.jpg' alt='' />
+            <p>NOMBRE</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              aut!
+            </p>
+          </section>
+
+          <section className='testimonios-perfiles'>
+            <Image height={400} src='/src/images/Testimonio2.jpg' alt='' />
+            <p>NOMBRE</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              aut!
+            </p>
+          </section>
+
+          <section className='testimonios-perfiles'>
+            <Image height={400} src='/src/images/Testimonio2.jpg' alt='' />
+            <p>NOMBRE</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+              aut!
+            </p>
+          </section>
+        </section>
+      </section>
+
+      <section className='mapa'>
+        <iframe
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27026.21769816903!2d-86.8695214378101!3d21.128267383112572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4c2be8f1679b3d%3A0x7ca6036dd487c1e5!2sTecNM%20-%20Campus%20Canc%C3%BAn!5e1!3m2!1ses-419!2smx!4v1728976651023!5m2!1ses-419!2smx'
+          width='100%'
+          height='450'
+          style='border:0;'
+          allowfullscreen=''
+          loading='lazy'
+          referrerpolicy='no-referrer-when-downgrade'
+        ></iframe>
+      </section>
+    </main>
   );
 }
