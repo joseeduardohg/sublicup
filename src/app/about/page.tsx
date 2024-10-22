0e58f9a3c045b4b407eec564e6e5f440267eb00e
Bris-678 <181124968+Bris-678@users.noreply.github.com>
2024-10-22 09:23:33 -0500

    añadir vista nosotros

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,26 @@
 import './page.css';
 export default function AboutPage() {
-  return <section></section>;
+  return (
+    <section>
+      <h2>Sobre Nosotros</h2>
+      <p>
+        En Sublicup, nos especializamos en la personalización de tazas, playeras
+        y termos, ofreciendo un servicio a domicilio para que puedas llevar tu
+        estilo a cualquier lugar.
+      </p>
+      <p>
+        Fundada en 2023, Sublicup nació de la pasión por el diseño personalizado
+        y el deseo de brindar a nuestros clientes productos únicos.
+      </p>
+      <p>
+        Nuestra misión es ofrecer productos de calidad que reflejen la
+        personalidad de nuestros clientes, todo con la comodidad de un servicio
+        a domicilio. Nos guiamos por valores de creatividad, calidad y un
+        compromiso excepcional hacia el cliente.
+      </p>
+      <p>
+        ¡Descubre nuestra variedad de productos y personaliza el tuyo hoy mismo!
+      </p>
+    </section>
+  );
 }
