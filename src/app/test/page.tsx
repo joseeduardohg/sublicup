4703cb86c37932b854ea277fc934207529c71965
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2024-10-30 10:20:02 -0500

    Add prisma studio script

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,20 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
-
 export default async function TestPage() {
   const prisma = new PrismaClient();
   const users = await prisma.user.findMany();
-  return <section>
-    <h1>Users</h1>
 
-    <ul>
-      {
-        users.map(user=> (
-          <li key={user.id}>
-            {user.name}
-          </li>
-        ))
-      }
-    </ul>
-  </section>;
-};
+  return (
+    <section>
+      <h1>Users</h1>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
